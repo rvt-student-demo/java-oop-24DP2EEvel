0f2bd6fcba9ff215a12a5c351fde9d36042dbc06
24DP2EEvel <a242329ee@rvt.lv>
2026-05-22 16:49:22 +0300

    InterfaceInABox  part 3 Book izpildīts.

---
+++ src/main/java/rvt/Box.java
@@ -0,0 +1,34 @@
+package rvt;
+
+import java.util.ArrayList;
+
+public class Box {
+    private double capacity;
+    private ArrayList<packable> items;
+
+    public Box(double capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(packable item) {
+        if (this.weight() + item.weight() <= this.capacity) {
+            this.items.add(item);
+        }
+    }
+
+    public double weight() {
+        double totalWeight = 0;
+
+        for (packable item : this.items) {
+            totalWeight = totalWeight + item.weight();
+        }
+
+        return totalWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Box: " + this.items.size() + " items, total weight " + this.weight() + " kg";
+    }
+}
